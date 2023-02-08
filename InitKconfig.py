00ad6580cd5a84043871f1b57b67d990fe123211
Gustavo Di Donato Salata <93167317+GustavoSalata@users.noreply.github.com>
2023-02-08 09:52:45 -0300

    Update InitKconfig.py

--- InitKconfig.py
+++ InitKconfig.py
@@ -10,6 +10,8 @@ os.system("apt install hydra -y")
 os.system("apt install nmap -y")
 os.system("apt install subfinder -y")
 os.system("apt install dnsx -y")
+os.system("apt install dirb -y")
+os.system("apt install gobuster -y")
 
 #Ferramentas externas essenciais
 os.system("wget https://raw.githubusercontent.com/julimar155/headergap/main/headergapv4.1.py")
