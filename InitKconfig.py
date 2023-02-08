1f938efeed0daf1748ee242e29d7f9f7d3dbddba
Gustavo Di Donato Salata <93167317+GustavoSalata@users.noreply.github.com>
2023-02-08 09:51:47 -0300

    Add files via upload

---
+++ InitKconfig.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import os
+
+#update na máquina 
+os.system("apt update -y")
+
+#Ferramentas internas essenciais
+os.system("apt install hydra -y")
+os.system("apt install nmap -y")
+os.system("apt install subfinder -y")
+os.system("apt install dnsx -y")
+
+#Ferramentas externas essenciais
+os.system("wget https://raw.githubusercontent.com/julimar155/headergap/main/headergapv4.1.py")
+
+
+
+#Worlists
+os.system("apt install seclists -y")
